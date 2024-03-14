d1c1ce45a771d1b5c706c5847f1113b80bd65113
muhammad rifky <muhammadrifkyramadhan434>
2024-03-14 18:10:24 +0700

    Add project files.

---
+++ insertionsort/insertionsort.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+int arr[9];
+int n;
+
+void input() {
+	while (true) {
+		cout << "Masukkan banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "======================" << endl;
+	cout << "Masukkan  Elemen Array" << endl;
+	cout << "======================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
+}
+
+void InsertionSort() {
+	int i, temp, j;
+
+	for (i = 1; i <= n - 1; i++) {
+
+		temp = arr[i];
+
+		j = i - 1;
+
+		while (j >= 0 && arr[j] > temp)
+		{
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = temp;
+	}
+}
+
+void display() {
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Element Array yang telah tersusun" << endl;
+	cout << "====================" << endl;
+	cout << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j];
+		if (j < n - 1) {
+			cout << " --> ";
+		}
+	}
+	cout << endl;
+	cout << endl;
+	cout << "Jumlah pass = " << n - 1 << endl;
+	cout << endl;
+}
+
+int main()
+{
+	input();
+	InsertionSort();
+	display();
+
+	system("pause");
+	return 0;
+
+}
